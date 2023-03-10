7d4c39edd4c3316d8e2d8ee304ba25fef840c60a
StefanoCostanzoGitHub <50273854+StefanoCostanzoGitHub@users.noreply.github.com>
2023-03-10 16:18:12 +0100

    commit travis.yml file

--- prova/src/logic/Main.java
+++ prova/src/logic/Main.java
@@ -2,6 +2,7 @@ package logic;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+    	int a = 1;
+        System.out.println(a); 
     }
 }
